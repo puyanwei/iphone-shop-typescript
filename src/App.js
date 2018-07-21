b6f88578a3147ea2271e88efdee831a8232a5835
Puyan Wei <puyanwei@gmail.com>
2018-07-21 14:02:59 +0100

    Setup basic structure using placeholders. Import iPhone image to App.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,30 @@ class App extends Component {
 		console.log(this.state.iphone);
 	};
 	render() {
-		return <div className="App" />;
+		return (
+			<div className="App">
+				<aside>
+					<img
+						src={require('./images/Apple_iPhone_8_Space_Grey_WS2-full-product-front.png')}
+						alt="iPhone space grey"
+						style={{ width: '70%' }}
+					/>
+				</aside>
+				<main>
+					<h1>Apple iPhone 8</h1>
+					<h2>STARS COMPONENT</h2>
+					<p>
+						All glas design, advanced cameras, wireless charging and
+						a smart A11 Bionic chip. Intelligence never looked
+						better
+					</p>
+					<h2>COLOUR COMPONENT</h2>
+					<h2>CAPACITY COMPONENT</h2>
+					<p>from £1149 upfront cost</p>
+					<p>when you pay £43.20 a month</p>
+				</main>
+			</div>
+		);
 	}
 }
 
